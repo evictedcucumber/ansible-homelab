6cfc99811bcc8a8974124d6b626e89e8b89acde6
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-06-18 23:55:57 +0200

    feat: add tcyclops host and ssh-hardening

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Ansible config for my homelab";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+
+        config.allowUnfree = true;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "ansible-homelab";
+        packages = with pkgs; [python3 ansible];
+      };
+    });
+}
